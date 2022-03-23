b148be6c12bf59febae6f82245c3c48aedbeaca7
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-03-23 16:56:03 -0300

    session page axios and footer

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -1,22 +1,31 @@
 import { useParams } from "react-router-dom";
+import React from "react";
+import axios from "axios";
+
+function Footer(props){
+    const { image, name} = props;
+    return(
+        <div className="footer">
+            <div className="background"><img src={image} /></div>
+            <h1>{name}</h1>
+        </div>
+    )
+}
+
+
 
 export default function MoviePage(){
     const { filmId } = useParams();
+    const [sections, setSections] = React.useState([]);
+
+    React.useEffect(() => {
+        let promisse = axios.get(`https://mock-api.driven.com.br/api/v5/cineflex/movies/${filmId}/showtimes`);
+        promisse.then((response) => {setSections(response.data)});
+    }, []);
+        console.log(sections);
     return(
         <>
-        <div>{filmId}</div>
-        <div>{filmId}</div>
-        <div>{filmId}</div>
-        <div>{filmId}</div>
-        <div>{filmId}</div>
-        <div>{filmId}</div>
-        <div>{filmId}</div>
-        <div>{filmId}</div>
-        <div>{filmId}</div>
-        <div>{filmId}</div>
-        <div>{filmId}</div>
-        <div>{filmId}</div>
-        <div>{filmId}</div>
+            <Footer image={sections.posterURL} name={sections.title}/>
         </>
     )
 }
